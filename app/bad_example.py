b0792c7764c087efe764bcc8a81632d78d870a1b
Hatem <gadallah.hatem@gmail.com>
2026-09-12 10:00:24 +0400

    update response of health api

--- app/bad_example.py
+++ app/bad_example.py
@@ -1,11 +1,11 @@
-def get_user(user_id):
-    query = f"SELECT * FROM users WHERE id={user_id}"
-    result = db.execute(query)
-    return result
-
-def save_config():
-    try:
-        api_key = "sk-hardcoded-secret-12345"
-        write_to_disk(api_key)
-    except:
-        pass
+# def get_user(user_id):
+#     query = f"SELECT * FROM users WHERE id={user_id}"
+#     result = db.execute(query)
+#     return result
+#
+# def save_config():
+#     try:
+#         api_key = "sk-hardcoded-secret-12345"
+#         write_to_disk(api_key)
+#     except:
+#         pass
